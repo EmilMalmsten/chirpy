84779ba9392d5810c27307ec9ac5b48695a451e7
Emil Malmsten <emil_malmsten@hotmail.com>
2023-04-25 09:23:22 +0700

    add healthz endpoint

--- main.go
+++ main.go
@@ -4,12 +4,20 @@ import (
 	"net/http"
 )
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	corsMux := middlewareCors(mux)
 
 	fileServer := http.FileServer(http.Dir("."))
 	mux.Handle("/", fileServer)
+
+	mux.HandleFunc("/healthz", readinessHandler)
 
 	assetsServer := http.FileServer(http.Dir("./assets/"))
 	mux.Handle("/birbs/", http.StripPrefix("/birbs/", assetsServer))
